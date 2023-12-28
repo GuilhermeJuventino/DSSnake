16b8a2662c7397416e7b708fa4fba1e8658b5a99
Guilherme Juventino <guilhermejuventino@gmail.com>
2023-12-27 21:27:47 -0300

    Started working on snake segments and movement system

--- source/main.c
+++ source/main.c
@@ -6,12 +6,14 @@
 // http://www.nightfoxandco.com
 
 #include <stdio.h>
+#include <string.h>
 #include <nds.h>
 #include <gl2d.h>
 
 // Definitions that might be needed for later
 #define HALF_WIDTH (SCREEN_WIDTH / 2)
 #define HALF_HEIGHT (SCREEN_HEIGHT / 2)
+#define MAX_SEGMENTS (1000)
 
 // Struct definitions
 struct snakeHead
@@ -21,10 +23,19 @@ struct snakeHead
 	int width;
 	int height;
 	int color;
+	int size;
+	char* direction;
+};
+
+struct segment
+{
+	int x;
+	int y;
 };
 
 // Function declarations
-void draw(struct snakeHead head);
+void drawSnake(struct snakeHead head, struct segment segments[]);
+void moveSnake(struct snakeHead head, struct segment segments[]);
 
 int main(int argc, char **argv)
 {
@@ -42,6 +53,17 @@ int main(int argc, char **argv)
     head.width = 5;
     head.height = 5;
     head.color = RGB15(255, 255, 255);
+    head.direction = "DOWN";
+
+    int starting_size = 3;
+    head.size = starting_size;
+
+    struct segment segments[MAX_SEGMENTS];
+    segments[0].x = head.x;
+    segments[0].y = head.y - 10;
+
+    segments[1].x = head.x;
+    segments[1].y = head.y - 20;
 
     int keys;
 
@@ -50,8 +72,25 @@ int main(int argc, char **argv)
 	// Reading input
 	scanKeys();
 
-	keys = keysDown();
+	keys = keysHeld();
+	
+	if (keys & KEY_UP && strcmp(head.direction, "DOWN") != 0)
+	{
+			head.direction = "UP";
+	} else if (keys & KEY_DOWN && strcmp(head.direction, "UP") != 0)
+	{
+		head.direction = "DOWN";
+	}
 
+	if (keys & KEY_LEFT && strcmp(head.direction, "RIGHT") != 0)
+	{
+		head.direction = "LEFT";
+	} else if (keys & KEY_RIGHT && strcmp(head.direction, "LEFT") != 0)
+	{
+		head.direction = "RIGHT";
+	}
+	
+	/*
 	if (keys & KEY_UP)
 	{
 		head.y -= 5;
@@ -69,9 +108,12 @@ int main(int argc, char **argv)
 	{
 		head.x += 5;
 	}
+	*/
+    	printf("\n %s", head.direction);
 	
 	// Updating and drawing the screen
-	draw(head);
+	moveSnake(head, segments);
+	drawSnake(head, segments);
 	glFlush(0);
 	    
         // Wait for the screen refresh
@@ -83,13 +125,100 @@ int main(int argc, char **argv)
     return 0;
 }
 
-void draw(struct snakeHead head)
+void drawSnake(struct snakeHead head, struct segment segments[])
 {
 	glBegin2D(); // Set up GL for 2d mode
 		
 	//glBoxFilled(200, 10, 250, 180, RGB15(255, 255, 255));
+	// Drawing the snake's head
 	glBoxFilled(head.x - head.width, head.y - head.height, head.x + head.width, head.y + head.height, head.color);
+
+	// Drawing the snake's body
+	for (int i = 0; i < head.size - 1; i++)
+	{
+		glBoxFilled(segments[i].x - head.width, segments[i].y - head.height, segments[i].x + head.width, segments[i].y + head.height, head.color);
+	}
 
 	glEnd2D();
 }
 
+void moveSnake(struct snakeHead head, struct segment segments[])
+{
+	int prev_x = head.x;
+	int prev_y = head.y;
+
+	if (strcmp(head.direction, "UP") == 0)
+	{
+		head.y -= 5;
+
+		for (int i = 0; i < head.size - 1; i++)
+		{
+			int new_x = prev_x;
+			int new_y = prev_y;
+
+			int seg_prev_x = segments[i].x;
+			int seg_prev_y = segments[i].y;
+
+			segments[i].x = new_x;
+			segments[i].y = new_y;
+
+			prev_x = seg_prev_x;
+			prev_y = seg_prev_y;
+
+		}
+	} else if (strcmp(head.direction, "DOWN") == 0)
+	{
+		head.y += 5;
+
+		for (int i = 0; i < head.size - 1; i++)
+		{
+			int new_x = prev_x;
+			int new_y = prev_y;
+
+			int seg_prev_x = segments[i].x;
+			int seg_prev_y = segments[i].y;
+
+			segments[i].x = new_x;
+			segments[i].y = new_y;
+
+			prev_x = seg_prev_x;
+			prev_y = seg_prev_y;
+		}
+	} else if (strcmp(head.direction, "LEFT") ==  0)
+	{
+		head.x -= 5;
+
+		for (int i = 0; i < head.size - 1; i++)
+		{
+			int new_x = prev_x;
+			int new_y = prev_y;
+
+			int seg_prev_x = segments[i].x;
+			int seg_prev_y = segments[i].y;
+
+			segments[i].x = new_x;
+			segments[i].y = new_y;
+
+			prev_x = seg_prev_x;
+			prev_y = seg_prev_y;
+		}
+	} else if (strcmp(head.direction, "RIGHT") == 0)
+	{
+		head.x += 5;
+
+		for (int i = 0; i < head.size - 1; i++)
+		{
+			int new_x = prev_x;
+			int new_y = prev_y;
+
+			int seg_prev_x = segments[i].x;
+			int seg_prev_y = segments[i].y;
+
+			segments[i].x = new_x;
+			segments[i].y = new_y;
+
+			prev_x = seg_prev_x;
+			prev_y = seg_prev_y;
+		}
+	}
+}
